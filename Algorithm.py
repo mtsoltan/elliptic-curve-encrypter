2069927ea9202411a92a7bba00bdb6471af5c51f
mtsoltan <mahmoud.t.soltan@gmail.com>
2018-12-06 12:45:31 +0200

    Abstracted long comment to docs.txt. Built upon Algorithm.py.

--- Algorithm.py
+++ Algorithm.py
@@ -1,57 +1,82 @@
 """
 import ecdsa
 import base64
+hashlib.sha256(data).digest()
+"""
 import hashlib
-"""
-
-P_secp265k1 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F  # 256 bits field prime.
-A_secp265k1 = 0
-B_secp265k1 = 7
-G_secp265k1_x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798  # 256 bits base point x coord.
-G_secp265k1_y = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8  # 256 bits base point y coord.
-N_secp265k1 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141  # 256 bits order of base point.
-H_secp265k1 = 1  # The cofactor of base point.
-
-"""
-Rules:
-Kpub = Kpriv x G
-Private keys are randomly generated, usually iterative operation of a cryptographic hash function to a password.
-Hash functions are designed in such a way to provide a bitstream of sufficient length to form a key.
-
-G is used as the generator for public keys. Multiplication is straightforward through the double-and-add method.
-Private keys cannot be recreated since multiplication is irreversible.
+from os import urandom
+from typing import Dict
+from Curve import Curve
+from Mod import Mod
 
 
-0- Randomly generate Kpriv (int), and generate Kpub = Kpriv x G (point).
+secp256k1 = {
+    'P': 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F,  # 256 bit field prime.
+    'A': 0,  # 256 bit curve A paramter
+    'B': 7,  # 256 bit curve B paramter
+    'Gx': 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,  # 256 bit base point x coord.
+    'Gy': 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8,  # 256 bit base point y coord.
+    # N is the smallest positive integer such that N * G = O
+    'N': 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141,  # 256 bit order of base point.
+    # H is the ratio between the curve order #E(Fp) and the base point order n.
+    'H': 1   # The cofactor of base point.
+}
 
 
-For a fixed set of paramters T = (P, A, B, G, N, H), to sign a message, m (ECDSA):
-1- Select a random integer k, 1 < k <= N - 1.
-2- Compute r = (k * G).x mod N
-   * where multiplication is over the elliptic curve (A, B, P)
-   * where if r == 0 go to step 1.
-3- Compute inv(k) using xgcd, and SHA1(m) as a 160-bit integer.
-4- Compue s = inv(k) * (SHA1(m) + Kpriv * r) mod N
-   * where if s == 0 go to step 1.
-5- The signature is (r, s).
+class Algorithm:
+    ALPHANUM = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+    BASE = len(ALPHANUM)  # 58
 
-To verify the signature (r, s) given Kpub:
-1- Verify that r, s are integers in the interval [1, n - 1]
-2- Compute inv(s) using xgcd, and SHA1(m) as a 160-bit integer.
-3- Compute u1 = inv(s) * SHA1(m) mod N and u2 = inv(s) * r mod N
-4- Compute x = (u1 * G + u2 * Kpub).x mod N.
-5- Accept if x == r.
+    def __init__(self, paramters: Dict, password: str):
+        """
+        Initializes the algorithm with the specified paramters.
+        """
+        self.D = paramters
+        self.curve = Curve(self.D['A'], self.D['B'], self.D['P'])
+        self.G = (Mod(self.D['Gx'], self.D['P']), Mod(self.D['Gy'], self.D['P']))
+        self.kpriv = self.int2xsha256(password)
+        self.kpub = self.curve.multiply(self.G, self.kpriv)
 
+    @staticmethod
+    def int2xsha256(text: str) -> int:
+        return int(hashlib.sha256(hashlib.sha256(text).digest()).hexdigest(), 16)
 
-For a fixed set of paramters T = (P, A, B, G, N, H), to encrypt a message, m given Kpub:
-(https://onlinelibrary.wiley.com/doi/pdf/10.1002/sec.1702):
-1- Select a random integer k, 1 < k <= N - 1.
-2- Compute R = k * G
-2- Convert the message to points on the elliptic curve, each Pi.
-3- Encrypt each point in the message, Ci = Pi + k * Kpub
-4- Send R and the set C.
+    @staticmethod
+    def random_number(blen: int) -> int:  # len is in bits
+        blen = (blen & 111) + (blen >> 3)  # Divide by 8 and round up.
+        ord_array = map(ord, urandom(blen))
+        rv = 0
+        for e in ord_array:
+            rv += e
+            rv <<= 8
+        return rv & ((1 << blen) - 1)
 
-To decrypt the message (R, C) using Kpriv:
-1- Calculate Pi = Ci - Kpriv * R
+    def b58encode(self, v):
+        """
+        Encodes v, which is a string of bytes, to base58.
+        """
+        long_value = 0
+        for (i, c) in enumerate(v[::-1]):
+            long_value += (256**i) * ord(c)
+        result = ''
+        while long_value >= self.BASE:
+            div, mod = divmod(long_value, self.BASE)
+            result = self.ALPHANUM[mod] + result
+            long_value = div
+        result = self.ALPHANUM[long_value] + result
 
-"""
+        # Bitcoin does a little leading-zero-compression:
+        # leading 0-bytes in the input become leading-1s
+        n_pad = 0
+        for c in v:
+            if c == '\0':
+                n_pad += 1
+            else:
+                break
+        return self.ALPHANUM[0] * n_pad + result
+
+    def sign(self, message: str) -> str:
+        return ''
+
+    def verify(self, message: str, signature: str) -> bool:
+        return False
